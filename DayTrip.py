1ad8231a1bd21f0a5c02edf9d746e5a1c8795b14
Christian Square <christiansquare20@gmail.com>
2022-10-21 17:05:49 -0500

    more work to do on my day trip list

---
+++ DayTrip.py
@@ -0,0 +1,45 @@
+import random
+city_destination=("Baton Rouge Louisiana", "New Orleans Louisiana", "Miami Florida")
+print(random.choices(city_destination))
+print("Miami Florida'")
+
+import random
+restaurants_destinaton=("Casa Tua", "Ceccini's", "Scarpetta")
+print(random.choice(restaurants_destinaton))
+print("Ceccini's")
+
+
+import random
+transportation_destination=("Car","Bus","Plane")
+print(random.choice(transportation_destination))
+print("Bus")
+
+import random
+entertainment_destination=("FunDimension", "Axe Habits Miami", "Jungle Island")
+print(random.choice(entertainment_destination))
+print("Jungle Island")
+
+
+import random
+city_destination=("Biloxi Mississippi", "San Antonio Texas", "Atlanta Georgia")
+print(random.choice(city_destination))
+print("Biloxi Mississippi")
+
+import random
+restaurants_destinaton=("Shaggy Biloxi Beach", "Fillin' Station", "Half Shell Oyster House")
+print(random.choice(restaurants_destinaton))
+print("Fillin' Station")
+
+import random
+transportation_destination=("Train", "Bus", "Motorcycle")
+print(random.choice(transportation_destination))
+print("train")
+
+import random
+entertainment_destinatinon=("Golden Nugget Casino","Harrah's Gulf Coast","Beau Riavage Theater")
+print(random.choice(entertainment_destinatinon))
+print("Harrah's Gulf Coast")
+print("if I dont like the previous choice, I have a back up.")
+print("My completed trip")
+
+
